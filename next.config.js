0c937222a3c9e8c14894a885a779531934846f2b
devildani <41055001+devildani@users.noreply.github.com>
2021-09-02 22:08:07 +0530

    Chnages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  images: {
-    domains: ["picsum.photos", "images.unsplash.com"],
-  },
+    images: {
+        domains: ["picsum.photos", "images.unsplash.com", "mansikasar.s3.amazonaws.com"],
+    },
 };
