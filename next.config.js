fa68d2b3d58ad6bd78af647543910a87c3baf248
devildani <41055001+devildani@users.noreply.github.com>
2021-09-04 12:40:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ["picsum.photos", "images.unsplash.com", "mansikasar.s3.amazonaws.com"],
+        domains: ["picsum.photos", "images.unsplash.com", "mansikasar.s3.amazonaws.com", "fvhmvntslgwafdnvywpe.supabase.in"],
     },
 };
