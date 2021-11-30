49072acf76dfcf2a307235ad62d9095624df6937
devildani <41055001+devildani@users.noreply.github.com>
2021-11-30 20:01:45 +0530

    Images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ["picsum.photos", "images.unsplash.com", "mansikasar.s3.amazonaws.com", "fvhmvntslgwafdnvywpe.supabase.in"],
+        domains: ["picsum.photos", "images.unsplash.com", "mansikasar.s3.amazonaws.com", "mansikasar-website.s3.ap-south-1.amazonaws.com"],
     },
 };
