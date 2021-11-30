263a241e78c177cbb09b0789b8a7ca59239b8d2c
devildani <41055001+devildani@users.noreply.github.com>
2021-11-30 11:51:38 +0530

    Changes

--- components/data/projectData.data.js
+++ components/data/projectData.data.js
@@ -1,14 +1,24 @@
 //contentSrc = main case study file
 //thumbnailsrc= thumbnail for uiux case study index page
 export const projectData = [{
+        title: "Bespoke",
+        content: "A website that offers a large selection of fashion items, all of which are tailored to the user's height and other body measurements.",
+        cta: "View",
+        category: "UX Design, Website",
+        projLink: "/projects/casestudies/0",
+        liveLink: " https://www.behance.net/gallery/124166733/Case-Study-noQ-Virtual-Slot-booking-avoid-crowd",
+        imgSrc: "/assets/all-projects/uixCaseStudy/0.png",
+        next: [1, 2]
+    },
+    {
         title: "B2BSouq Case Study",
         content: "Redesigned a Dubai-based business buying and selling platform to enhance the website's architecture and give it a more modern look.",
         cta: "View",
         category: "UX Design, Re-Design",
         projLink: "/projects/casestudies/0",
         liveLink: " https://www.behance.net/gallery/124166733/Case-Study-noQ-Virtual-Slot-booking-avoid-crowd",
-        imgSrc: "/assets/all-projects/uixCaseStudy/0.png",
-        next: [1, 2]
+        imgSrc: "/assets/all-projects/uixCaseStudy/1.png",
+        next: [0, 2]
     },
     {
         title: "VES noQ Case Study",
@@ -17,8 +27,8 @@ export const projectData = [{
         category: "App, UX Design",
         projLink: "/projects/casestudies/1",
         liveLink: "https://www.behance.net/gallery/124166733/Case-Study-noQ-Virtual-Slot-booking-avoid-crowd",
-        imgSrc: "/assets/all-projects/uixCaseStudy/1.png",
-        next: [0, 2]
+        imgSrc: "/assets/all-projects/uixCaseStudy/2.png",
+        next: [0, 1]
     },
     {
         title: "Proj.co Case Study",
@@ -27,7 +37,7 @@ export const projectData = [{
         category: "UX Design, Collaboration",
         projLink: "/projects/casestudies/2",
         liveLink: "https://www.behance.net/gallery/124166733/Case-Study-noQ-Virtual-Slot-booking-avoid-crowd",
-        imgSrc: "/assets/all-projects/uixCaseStudy/2.png",
+        imgSrc: "/assets/all-projects/uixCaseStudy/3.png",
         next: [0, 1]
     },
 ];
