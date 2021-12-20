e0bd8301948fe299698ce804d55aa4c81d0246e6
Pratik Dani <pratikdani@Pratiks-MacBook-Air.local>
2021-12-20 19:59:59 +0530

    Update HeroComp.jsx

--- components/HomeView/HeroComp.jsx
+++ components/HomeView/HeroComp.jsx
@@ -24,7 +24,7 @@ const heroSection = () => {
           </p> */}
           <h1 className="px-4 mb-6 text-3xl font-playfair font-semibold space lg:text-5xl text-heroH text-center">
             Hey! I&#39;m <span className="text-brand">Mansi Kasar</span>, <br />
-            a UI/UX Designer.
+            a UX/UI Designer.
           </h1>
           <p className="mb-6 text-base font-normal text-heroH lg:mb-4 lg:text-lg text-center font-poppins">
             Focused on creating beautiful user experiences through Design.
